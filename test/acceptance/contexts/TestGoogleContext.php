1b6d77b9fac7874fb41df5eacbea15affefdb2b3
long nguyen <nguyentienlong88@gmail.com>
2015-12-15 22:12:09 +0700

    add hook

--- test/acceptance/contexts/TestGoogleContext.php
+++ test/acceptance/contexts/TestGoogleContext.php
@@ -1,9 +1,38 @@
 <?php
 
 use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Hook\Scope\BeforeFeatureScope;
+use Behat\Behat\Hook\Scope\AfterFeatureScope;
 
 class TestGoogleContext extends MinkContext
 {
+    /** @BeforeFeature */
+    public static function setupFeature(BeforeFeatureScope $scope)
+    {
+        if($scope->getSuite()->getSetting('seed_data')) {
+            echo('setting up database');
+            //call sh script or command
+            echo('finished setup database');
+        }
+    }
+
+    /** @AfterFeature */
+    public static function teardownFeature(AfterFeatureScope $scope) {
+        echo('dropping database');
+        //call sh script or command to drop db
+        echo('database dropped');
+        //may be need to trigger and event to some listener
+        echo('feature tested, plz check blah blah blah');
+    }
+
+    /** @BeforeScenario */
+    public function beforeScenario($event) {
+    }
+
+    /** @AfterScenario */
+    public function afterScenario($event) {
+    }
+
     /**
     * @Then /^I wait for the next step$/
     */
